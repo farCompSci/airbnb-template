476b99082d95746a49485d63a22b176bda18f76e
Farjad_Mada <88432392+farCompSci@users.noreply.github.com>
2024-04-08 18:53:38 -0700

    removed some stuff

--- src/functions.php
+++ src/functions.php
@@ -117,11 +117,6 @@ function getAmenities($db,$accommodates,$extId){
     return $rows;
 }
 
-define("SERVER", "localhost");
-define("PORT", "3306");
-define("USERNAME", "fakeAirbnbUser");
-define("PASSWORD", "apples11Million!");
-define("DATABASE", "fakeAirbnb");
 
 
 function dbConnect(){
